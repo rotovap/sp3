967c31109df464fdc97cfd3994a04af0902c3d82
rotovap <>
2024-01-14 19:03:02 +0100

    FE: fix rdkit loading not being awaited causing undefined window.RDKit
    component using RDKit would try to render before RDKit is initialized
    upon window refresh. Then/catch was not working for some reason.
    Switching to await and using a try/catch seems to work

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -11,17 +11,20 @@ declare global {
     }
 }
 
-window
-    .initRDKitModule()
-    .then((instance: RDKitModule) => {
-        console.log("RDKit version: ", instance.version())
-        window.RDKit = instance;
-        return instance;
-    })
-    .catch((e) => {
-        console.error("Error loading RDKit");
-        console.error(e)
-    });
+
+// IMPORTANT: need this await here!
+// otherwise, this will run asynchrounously and then components needing RDKit
+// will be rendered before RDKit has loaded!
+// for some reason, using then and catch was not working
+try {
+    const instance: RDKitModule = await window.initRDKitModule()
+    console.log("RDKit version: ", instance.version())
+    window.RDKit = instance
+
+} catch (e) {
+    console.error("Error loading RDKit");
+    console.error(e)
+}
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
