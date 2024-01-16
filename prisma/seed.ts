8713570db77fab0ae0a302d6108629ec90742896
rotovap <>
2024-01-16 16:03:55 +0100

    Add reagents for an example suzuki coupling

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -110,7 +110,15 @@ const seedReagents = async () => {
         ('ethanol', 'CCO', 46.07, 0.79),
         ('butane', 'CCCC', 58.12, 0.6),
         ('thf', 'C1CCOC1', 72.11, 0.888),
-        ('diethyl(3-pyridyl)borane', 'CCB(CC)c1cccnc1', 147.03, NULL)
+        ('diethyl(3-pyridyl)borane', 'CCB(CC)c1cccnc1', 147.03, NULL),
+        ('1-bromo-3-(methylsulfonyl)benzene','CS(=O)(=O)c1cccc(Br)c1', 235.10, NULL),
+        ('Pd(PPh3)4', 
+            '[Pd]([P](c1ccccc1)(c1ccccc1)c1ccccc1)([P](c1ccccc1)(c1ccccc1)c1ccccc1)([P](c1ccccc1)(c1ccccc1)c1ccccc1)[P](c1ccccc1)(c1ccccc1)c1ccccc1',
+            1154.26803,
+            NULL),
+        ('tbab', 'CCCC[N+](CCCC)(CCCC)CCCC.[Br-]', 322.37, NULL),
+        ('K2CO3', 'O=C([O-])[O-].[K+].[K+]', 138.205, NULL),
+        (NULL, 'CS(=O)(=O)c1cccc(-c2cccnc2)c1', 233.05104, NULL)
 `;
 };
 
@@ -133,6 +141,36 @@ const seedAssignReagentToExperiments = async () => {
         reagentId: 4,
         experimentId: 2,
         reactionSchemeLocation: "LEFT_SIDE",
+        equivalents: 1,
+      },
+      {
+        reagentId: 5,
+        experimentId: 2,
+        reactionSchemeLocation: "LEFT_SIDE",
+        equivalents: 1,
+      },
+      {
+        reagentId: 6,
+        experimentId: 2,
+        reactionSchemeLocation: "ABOVE_ARROW",
+        equivalents: 1,
+      },
+      {
+        reagentId: 7,
+        experimentId: 2,
+        reactionSchemeLocation: "BELOW_ARROW",
+        equivalents: 1,
+      },
+      {
+        reagentId: 8,
+        experimentId: 2,
+        reactionSchemeLocation: "BELOW_ARROW",
+        equivalents: 1,
+      },
+      {
+        reagentId: 9,
+        experimentId: 2,
+        reactionSchemeLocation: "RIGHT_SIDE",
         equivalents: 1,
       },
     ],
