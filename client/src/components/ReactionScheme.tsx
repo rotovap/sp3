a374a16a4d80ba3cb414aef6f4beeb939841ce0c
rotovap <>
2024-01-17 11:23:55 +0100

    FE: fix each child needs unique prop error
    need to put the key on the root fragment. Was using <> and </> and
    switched to <div key={id}></div> so that the key can be added in div

--- client/src/components/ReactionScheme.tsx
+++ client/src/components/ReactionScheme.tsx
@@ -1,6 +1,6 @@
 import { Grid, Stack, Typography } from "@mui/material"
 import MoleculeStructure from "./MoleculeStructure/MoleculeStructure"
-import { ExperimentWithReagents, ReagentInExperiment, ReagentWithSMILES } from "../../../server/routes/experiments"
+import { ExperimentWithReagents, ReagentWithSMILES } from "../../../server/routes/experiments"
 import { ArcherContainer, ArcherElement } from 'react-archer';
 
 interface Props {
@@ -19,40 +19,38 @@ interface LeftAndRightSideProps {
 // + signs. They are similar
 const LeftAndRightSide = ({ reagent, idx, lengthOfReagents }: LeftAndRightSideProps) => {
     return (
-        <>
-            <Stack key={idx} direction="row">
-                {/* this grid is to help align the items in the stack 
+        <Stack direction="row" key={reagent.id}>
+            {/* this grid is to help align the items in the stack 
                         so that the + sign height is in the middle of the 
                         molecules */}
-                <Grid
-                    container
-                    spacing={0}
-                    direction="row"
-                    alignItems="center"
-                    justifyContent="center"
-                >
-                    <Stack padding={3} direction="column" textAlign="center">
-                        <MoleculeStructure
-                            id="molecule-structure"
-                            structure={reagent.canonicalSMILES}
-                            width={MOL_STRUCT_DIM}
-                            height={MOL_STRUCT_DIM}
-                            svgMode
-                        />
-                        <Typography variant="caption" display="block" gutterBottom>
-                            {reagent.id}
-                        </Typography>
+            <Grid
+                container
+                spacing={0}
+                direction="row"
+                alignItems="center"
+                justifyContent="center"
+            >
+                <Stack padding={3} direction="column" textAlign="center">
+                    <MoleculeStructure
+                        id="molecule-structure"
+                        structure={reagent.canonicalSMILES}
+                        width={MOL_STRUCT_DIM}
+                        height={MOL_STRUCT_DIM}
+                        svgMode
+                    />
+                    <Typography variant="caption" display="block" gutterBottom>
+                        {reagent.id}
+                    </Typography>
 
+                </Stack>
+                {idx < lengthOfReagents - 1 ?
+                    <Stack>
+                        +
                     </Stack>
-                    {idx < lengthOfReagents - 1 ?
-                        <Stack>
-                            +
-                        </Stack>
-                        : null
-                    }
-                </Grid>
-            </Stack>
-        </>
+                    : null
+                }
+            </Grid>
+        </Stack>
     )
 }
 
@@ -68,18 +66,16 @@ export const ReactionScheme = ({ experiment }: Props) => {
             <ArcherContainer>
                 <Grid
                     container
-                    xs={12}
                     direction="row"
                     alignItems="center"
                     justifyContent="center">
                     {leftSideReagents.map((i, idx) => {
                         return (
-                            <>
+                            <div key={i.reagentId}>
                                 {/* draw the arrow between the last left side reagent and the first right side reagent*/}
                                 {
                                     idx === leftSideReagents.length - 1 ?
                                         <ArcherElement
-                                            key={idx}
                                             id={`left-side-${idx}`}
                                             relations={[
                                                 {
@@ -95,16 +91,16 @@ export const ReactionScheme = ({ experiment }: Props) => {
                                             </div>
                                         </ArcherElement>
                                         :
-                                        <LeftAndRightSide key={idx} idx={idx} reagent={i.reagent} lengthOfReagents={leftSideReagents.length} />
+                                        <LeftAndRightSide idx={idx} reagent={i.reagent} lengthOfReagents={leftSideReagents.length} />
                                 }
-                            </>
+                            </div>
                         )
                     })}
                     <Stack spacing={3}>
                         <Stack spacing={0}>
-                            {aboveArrowReagents.map((i, idx) => {
+                            {aboveArrowReagents.map((i) => {
                                 return (
-                                    <Stack key={idx} marginLeft={REACTION_ARROW_MARGIN_LEFT}>
+                                    <Stack key={i.reagentId} marginLeft={REACTION_ARROW_MARGIN_LEFT}>
                                         <Typography variant="caption" display="block" gutterBottom>
                                             {i.reagent.name}
                                         </Typography>
@@ -113,9 +109,9 @@ export const ReactionScheme = ({ experiment }: Props) => {
                             })}
                         </Stack>
                         <Stack paddingTop={2} spacing={0}>
-                            {belowArrowReagents.map((i, idx) => {
+                            {belowArrowReagents.map((i) => {
                                 return (
-                                    <Stack key={idx} marginLeft={REACTION_ARROW_MARGIN_LEFT}>
+                                    <Stack key={i.reagentId} marginLeft={REACTION_ARROW_MARGIN_LEFT}>
                                         <Typography variant="caption" display="block" gutterBottom>
                                             {i.reagent.name}
                                         </Typography>
@@ -127,18 +123,17 @@ export const ReactionScheme = ({ experiment }: Props) => {
                     </Stack>
                     {rightSideReagents.map((i, idx) => {
                         return (
-                            <>
+                            <div key={i.reagentId}>
                                 {idx === 0 ?
-                                    <ArcherElement id={`right-side-0`} key={idx}>
+                                    <ArcherElement id={`right-side-0`}>
                                         <div style={boxStyle}>
-                                            <LeftAndRightSide idx={idx} reagent={i.reagent} lengthOfReagents={rightSideReagents.length} />
+                                            <LeftAndRightSide idx={i.reagentId} reagent={i.reagent} lengthOfReagents={rightSideReagents.length} />
                                         </div>
                                     </ArcherElement>
                                     :
-                                    <LeftAndRightSide key={idx} idx={idx} reagent={i.reagent} lengthOfReagents={rightSideReagents.length} />
-
+                                    <LeftAndRightSide idx={idx} reagent={i.reagent} lengthOfReagents={rightSideReagents.length} />
                                 }
-                            </>
+                            </div>
                         )
                     })}
                 </Grid >
