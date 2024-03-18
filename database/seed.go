7deddf2f9668213dbeec13df905f92d1088968fe
rotovap <rotovapp@gmail.com>
2024-03-18 13:48:27 +0100

    fix reagent names in seed script

--- database/seed.go
+++ database/seed.go
@@ -26,16 +26,17 @@ func seedExperiments(db *sql.DB) {
 }
 
 func seedReagents(db *sql.DB) {
-	reagents := `('ethanol', 'CCO', 46.07, 0.79),
+	reagents := `
+        ('ethanol', 'CCO', 46.07, 0.79),
         ('butane', 'CCCC', 58.12, 0.6),
-        ('thf', 'C1CCOC1', 72.11, 0.888),
+        ('THF', 'C1CCOC1', 72.11, 0.888),
         ('diethyl(3-pyridyl)borane', 'CCB(CC)c1cccnc1', 147.03, NULL),
         ('1-bromo-3-(methylsulfonyl)benzene','CS(=O)(=O)c1cccc(Br)c1', 235.10, NULL),
         ('Pd(PPh3)4', 
             '[Pd]([P](c1ccccc1)(c1ccccc1)c1ccccc1)([P](c1ccccc1)(c1ccccc1)c1ccccc1)([P](c1ccccc1)(c1ccccc1)c1ccccc1)[P](c1ccccc1)(c1ccccc1)c1ccccc1',
             1154.26803,
             NULL),
-        ('tbab', 'CCCC[N+](CCCC)(CCCC)CCCC.[Br-]', 322.37, NULL),
+        ('TBAB', 'CCCC[N+](CCCC)(CCCC)CCCC.[Br-]', 322.37, NULL),
         ('K2CO3', 'O=C([O-])[O-].[K+].[K+]', 138.205, NULL),
         (NULL, 'CS(=O)(=O)c1cccc(-c2cccnc2)c1', 233.05104, NULL)`
 
